348ce30a96b5c6d612d4fd11373051945db382f2
Giorgi <giorgikarchiladze5@gmail.com>
2024-07-26 18:01:55 +0400

    Updates
    Added user input, additional verification (whether the card consists of 16 characters and if all of them are digits) and an exit keyword to close the program.

--- luhnAlgorithm/python.py
+++ luhnAlgorithm/python.py
@@ -1,31 +1,45 @@
-#Luhn Algorithm
+#Luhn Algorithm 1111111111111111
+
 def verify_card_number(card_number):
-    sum_of_odd_digits = 0
-    card_number_reversed = card_number[::-1]
-    odd_digits = card_number_reversed[::2]
+    card_translation = str.maketrans({"-": '', " ": ''})
+    translated_card_number = card_number.translate(card_translation)
+    if len(translated_card_number) != 16 or not translated_card_number.isdigit():
+        #random return value to have for other options other than True and False
+        return 'sad'
+    else:
+        sum_of_odd_digits = 0
+        card_number_reversed = translated_card_number[::-1]
+        odd_digits = card_number_reversed[::2]
 
-    for digit in odd_digits:
-        sum_of_odd_digits += int(digit)
+        for digit in odd_digits:
+            sum_of_odd_digits += int(digit)
 
-    sum_of_even_digits = 0
-    even_digits = card_number_reversed[1::2]
-    for digit in even_digits:
-        number = int(digit) * 2
-        if number >= 10:
-            number = (number // 10) + (number % 10)
-        sum_of_even_digits += number
-    total = sum_of_odd_digits + sum_of_even_digits
-    print(total)
-    return total % 10 == 0
+        sum_of_even_digits = 0
+        even_digits = card_number_reversed[1::2]
+        for digit in even_digits:
+            number = int(digit) * 2
+            if number >= 10:
+                number = (number // 10) + (number % 10)
+            sum_of_even_digits += number
+        total = sum_of_odd_digits + sum_of_even_digits
+        return total % 10 == 0
+
 
 def main():
-    card_number = '4111-1111-4555-1141'
-    card_translation = str.maketrans({'-': '', ' ': ''})
-    translated_card_number = card_number.translate(card_translation)
+    print('Type \'exit\' to close the program')
+    while True:
+        custom_input = input('Enter a card number: \n')
+        if custom_input == 'exit':
+            break
+        result = verify_card_number(custom_input)
+        if result == 'sad':
+            print('Card numbers consist only of digits and only 16 of them!')
+        elif result:
+            print('Valid!')
+            break
+        else:
+            print('Invalid!')
+            break
 
-    if verify_card_number(translated_card_number):
-        print('VALID!')
-    else:
-        print('INVALID!')
 
 main()
